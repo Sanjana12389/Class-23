88d04fc866d2597bdb86a7380d98227e5e644467
Sanjana12389 <71740846+Sanjana12389@users.noreply.github.com>
2020-10-14 18:39:30 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1,box2,ground;
+
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1=new Box(200,100,50,50);
+    box2=new Box(240,300,50,50);
+ground=new Ground(200,380,390,50);
+  
+
+   
+}
+
+function draw(){
+    background("purple");
+    Engine.update(engine);
+    rectMode(CENTER);
+
+    ground.display();
+  box1.display();
+  box2.display();
+
+
+
+}
